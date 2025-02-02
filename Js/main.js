fda39afa89a85ffc87fde78dd2450dc133a4df1d
Carolina <carolinaxlad@gmail.com>
2025-02-02 17:58:39 -0500

    comentario desde otra branch

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.nav-menu');
     console.log("esto es un proyecto nuevo"); //este es un comentario
+    console.log("esto es un comentario que viene de otra branch"); 
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('nav-active');
     });
