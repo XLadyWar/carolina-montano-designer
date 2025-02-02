d2b747315d301abd0f0b02fdc14c4acc0292fcf3
Carolina <carolinaxlad@gmail.com>
2025-02-02 17:28:46 -0500

    este es un primer commit

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.nav-menu');
-
+    console.log("esto es un proyecto nuevo");
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('nav-active');
     });
