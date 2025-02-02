fc1287a3175a7e23b76925f6d939b718d0484a9a
Carolina <carolinaxlad@gmail.com>
2025-02-02 17:46:36 -0500

    nevo comentario añadido

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.nav-menu');
-    console.log("esto es un proyecto nuevo");
+    console.log("esto es un proyecto nuevo"); //este es un comentario
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('nav-active');
     });
