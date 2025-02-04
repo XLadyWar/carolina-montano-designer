589eb1cb6b8e752d75249aa67f0c91f9f99f42b1
Carolina <carolinaxlad@gmail.com>
2025-02-04 16:34:38 -0500

    fixed imagen yo

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.nav-menu');
-    console.log("esto es un proyecto nuevo"); //este es un comentario
-    console.log("esto es un comentario nuevo"); //este es un comentario
-    console.log("esto es un comentario que viene de otra branch"); 
+
     hamburger.addEventListener('click', () => {
         navMenu.classList.toggle('nav-active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textoMayor = document.querySelector('.texto-mayor');
+    const heroText = document.querySelector('.hero-text');
+
+    setTimeout(() => {
+        textoMayor.classList.add('visible');
+    }, 300); // Aparece primero el texto mayor
+
+    setTimeout(() => {
+        heroText.classList.add('visible');
+    }, 600); // Luego aparece el hero text con un ligero retraso
+});
+
+
